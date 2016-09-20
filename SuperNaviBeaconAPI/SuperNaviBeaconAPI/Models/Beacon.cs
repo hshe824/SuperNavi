d61e8d596e1d8885d9c1e22133d5be9b01ad6428
Greggory Tan <greggory_737@live.com>
2016-09-20 21:06:41 +1200

    Removed default controllers that were not needed. Working on implementing Beacon api calls

---
+++ SuperNaviBeaconAPI/SuperNaviBeaconAPI/Models/Beacon.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperNaviBeaconAPI.Models
+{
+    public class Beacon : TableEntity 
+    {
+        public Beacon(String ID) {
+            this.PartitionKey = "Beacon";
+            this.RowKey = ID;
+        }
+        public Beacon() { }
+        public String ID { get; set; }
+        public int positionX { get; set; }
+        public int positionY { get; set; }
+    }
+}
