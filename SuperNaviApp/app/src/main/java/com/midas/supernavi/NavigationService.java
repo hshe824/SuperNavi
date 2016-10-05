22ec68aac6cabd76c8d84ef2b25343192d39b284
hshe824 <hshe824@aucklanduni.ac.nz>
2016-10-05 16:58:15 +1300

    Added navigation service skeleton

---
+++ SuperNaviApp/app/src/main/java/com/midas/supernavi/NavigationService.java
@@ -0,0 +1,48 @@
+package com.midas.supernavi;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.Region;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+import org.altbeacon.beacon.BeaconConsumer;
+
+import java.util.ArrayList;
+
+
+public class NavigationService extends Service implements BeaconConsumer  {
+    public NavigationService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+    }
+
+
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+                Iterator<Beacon> iterator = collection.iterator();
+                while(iterator.hasNext()){
+                    Beacon beacon = iterator.next();
+                    Log.d(TAG, "Beacon ID1: " + beacon.getId1());
+                    Log.d(TAG, "Beacon ID2: " + beacon.getId2());
+                    Log.d(TAG, "Beacon ID3: " + beacon.getId3());
+                    Log.d(TAG, "Beacon ID3: " + beacon.getRssi());
+                }
+                currentBeaconList = new ArrayList<>(collection);
+            }
+        });
+
+        try{
+            beaconManager.startRangingBeaconsInRegion(new Region("defaultRegion", null, null, null));
+        }catch(RemoteException e){
+
+        }
+    }
+}
