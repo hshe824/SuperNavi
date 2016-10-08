99e96591af6a836c906c98bb8ca3992dc6d1a562
hshe824 <hshe824@aucklanduni.ac.nz>
2016-10-09 01:17:02 +1300

    Refactored and added more help commands

--- SuperNaviApp/app/src/main/java/com/midas/supernavi/ProductSelection.java
+++ SuperNaviApp/app/src/main/java/com/midas/supernavi/ProductSelection.java
@@ -68,13 +68,45 @@ public class ProductSelection extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_selection);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        populateListView();
+        //Default mode is product selection
+        setTitle("SuperNavi - Product Selection");
+        currentOperatingMode = OperatingMode.PRODUCT_SELECTION;
+        modeSelector = (VerticalSeekBar) findViewById(R.id.modeSelector);
+        modeSelector.setOnSeekBarChangeListener(new modeListener());
+        Button speakCommand = (Button) findViewById(R.id.speakCommand);
+        speakCommand.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                displaySpeechRecognizer();
+            }
+        });
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.ENGLISH);
+                }
+            }
+        });
+        textToSpeech.setSpeechRate((float) 0.85);
+        textToSpeech.speak("Press speak button on the bottom of the screen and say an item to add it to the shopping list",TextToSpeech.QUEUE_FLUSH, null, null);
+
+
+    }
+
     //Handles product selection mode
     private void productSelection() {
         currentOperatingMode = OperatingMode.PRODUCT_SELECTION;
-        Log.d("Mode","Entering product selection mode");
+        Log.d("Mode", "Entering product selection mode");
         setTitle("SuperNavi - Product Selection");
-        textToSpeech.speak("Entering product selection mode",TextToSpeech.QUEUE_FLUSH, null,null);
-
+        tts("Entering product selection mode");
 
 
     }
@@ -82,9 +114,9 @@ public class ProductSelection extends AppCompatActivity {
     //Handles navigate mode
     private void navigate() {
         currentOperatingMode = OperatingMode.NAVIGATION;
-        Log.d("Mode","Entering navigation mode");
+        Log.d("Mode", "Entering navigation mode");
         setTitle("SuperNavi - Navigation");
-        textToSpeech.speak("Entering navigation mode",TextToSpeech.QUEUE_FLUSH, null,null);
+        tts("Entering navigation mode");
 
 
     }
@@ -93,13 +125,13 @@ public class ProductSelection extends AppCompatActivity {
     //Handles free roam mode
     private void freeRoam() {
         currentOperatingMode = OperatingMode.FREE_ROAM;
-        Log.d("Mode","Entering free roam mode");
+        Log.d("Mode", "Entering free roam mode");
         setTitle("SuperNavi - Free Roam");
-        textToSpeech.speak("Entering free roam mode",TextToSpeech.QUEUE_FLUSH, null,null);
-
+        tts("Entering free roam mode");
 
 
     }
+
     // Show google speech recognizer
     private void displaySpeechRecognizer() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
@@ -115,103 +147,94 @@ public class ProductSelection extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
             ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            Log.d("Matches:",matches.toString());
+            Log.d("Matches:", matches.toString());
             //Global commands to change mode etc
-            if (!checkCommand(matches)){
-
-            }
-
-
+            checkCommand(matches);
         }
     }
 
-    private boolean checkCommand(ArrayList<String> matches){
+    private boolean checkCommand(ArrayList<String> matches) {
         //Global commands
-        if (matches.contains("product selection mode") || matches.contains("product selection") ||matches.contains("selection") || matches.contains("select") || matches.contains("product")) {
+        if (matches.contains("product selection mode") || matches.contains("product selection") || matches.contains("selection") || matches.contains("select") || matches.contains("product")) {
             modeSelector.setProgress(0);
-            if (currentOperatingMode == OperatingMode.PRODUCT_SELECTION){
-                textToSpeech.speak("You are already in product selection mode!",TextToSpeech.QUEUE_FLUSH, null,null);
+            if (currentOperatingMode == OperatingMode.PRODUCT_SELECTION) {
+                tts("You are already in product selection mode!");
             }
             return true;
         } else if (matches.contains("navigate") || matches.contains("navigation") || matches.contains("navigation mode")) {
             modeSelector.setProgress(1);
-            if (currentOperatingMode == OperatingMode.PRODUCT_SELECTION){
-                textToSpeech.speak("You are already in navigation mode!",TextToSpeech.QUEUE_FLUSH, null,null);
+            if (currentOperatingMode == OperatingMode.PRODUCT_SELECTION) {
+                tts("You are already in navigation mode!");
             }
             return true;
         } else if (matches.contains("free roam mode") || matches.contains("free roam") || matches.contains("roam")) {
             modeSelector.setProgress(2);
-            if (currentOperatingMode == OperatingMode.PRODUCT_SELECTION){
-                textToSpeech.speak("You are already in free roam mode!",TextToSpeech.QUEUE_FLUSH, null,null);
+            if (currentOperatingMode == OperatingMode.PRODUCT_SELECTION) {
+                tts("You are already in free roam mode!");
             }
             return true;
-        } else if (matches.contains("where am i")){
+        } else if (matches.contains("where am i")) {
             //TODO: Tell user where they are
+            return true;
+        } else if (matches.contains("exit") || matches.contains("quit") || matches.contains("finished")) {
+            tts("Exiting app");
+            finish();
+            return true;
+        } else if (matches.contains("help")){
+            help(matches);
             return true;
         }
 
         //Mode specific:
 
         //Product selection commands
-        if (currentOperatingMode == OperatingMode.PRODUCT_SELECTION){
-            if (matches.contains("add") || matches.contains("new")){
-                textToSpeech.speak("What items would you like to add?",TextToSpeech.QUEUE_FLUSH, null,null);
-                displaySpeechRecognizer();
-            } else {
-                //Add items
-                addItems(matches);
-            }
+        if (currentOperatingMode == OperatingMode.PRODUCT_SELECTION) {
+            //Add items
+            addItems(matches);
         }
         return false;
     }
 
+    private void help(ArrayList<String> matches){
+        switch (currentOperatingMode){
+            case PRODUCT_SELECTION:
+                tts("You are in product selection mode. Simply click the speak button and say an item you want to add to your shopping list");
+                break;
+            case FREE_ROAM:
+                tts("You are in free roam mode. I will say what items you are passing in this supermarket as you walk around freely");
+                break;
+            case NAVIGATION:
+                tts("You are in navigation mode. I will guide you around to pick up all the items on your grocery list");
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid Mode");
+        }
+
+    }
+
+    private void tts(String toSpeak){
+        textToSpeech.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+
     //Add items to shopping list
-    private void addItems(ArrayList<String> matches){
-        if (!gList.contains(matches.get(0).toLowerCase())){
+    private void addItems(ArrayList<String> matches) {
+        if (!gList.contains(matches.get(0).toLowerCase())) {
             gList.add(matches.get(0));
             adapter.notifyDataSetChanged();
-            textToSpeech.speak("Added " + matches.get(0) + " to shopping list",TextToSpeech.QUEUE_FLUSH, null,null);
+            tts("Added " + matches.get(0) + " to shopping list");
         } else {
-            textToSpeech.speak("Your shopping list already contains "+matches.get(0),TextToSpeech.QUEUE_FLUSH, null,null);
+            tts("Your shopping list already contains "+ matches.get(0));
         }
     }
-        @Override
-        protected void onCreate (Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_product_selection);
-            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-            setSupportActionBar(toolbar);
-            populateListView();
-            //Default mode is product selection
-            setTitle("SuperNavi - Product Selection");
-            currentOperatingMode = OperatingMode.PRODUCT_SELECTION;
-            modeSelector = (VerticalSeekBar) findViewById(R.id.modeSelector);
-            modeSelector.setOnSeekBarChangeListener(new modeListener());
-            Button speakCommand = (Button) findViewById(R.id.speakCommand);
-            speakCommand.setOnClickListener(new Button.OnClickListener() {
-                public void onClick(View v) {
-                    displaySpeechRecognizer();
-                }
-            });
 
-            textToSpeech=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-                @Override
-                public void onInit(int status) {
-                    if(status != TextToSpeech.ERROR) {
-                        textToSpeech.setLanguage(Locale.ENGLISH);
-                    }
-                }
-            });
-            textToSpeech.setSpeechRate((float)0.75);
 
-        }
 
-        //Creates grocery list
+    //Creates grocery list
     private void populateListView() {
         String[] groceries = {"bananas", "milk", "steak", "lettuce", "chips", "bread"};
         gList = new ArrayList<String>(Arrays.asList(groceries));
         adapter = new ArrayAdapter<String>(this, R.layout.groceries, gList);
-        ListView groceryListView= (ListView) findViewById(groceryList);
+        ListView groceryListView = (ListView) findViewById(groceryList);
         groceryListView.setAdapter(adapter);
     }
 
