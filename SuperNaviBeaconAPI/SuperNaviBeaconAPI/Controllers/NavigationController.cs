6cf400202426415c7f1102295123cdc1a590c234
Jay Kim <jkim506@aucklanduni.ac.nz>
2016-10-08 13:02:00 +1300

    Navigation API/Model set up

--- SuperNaviBeaconAPI/SuperNaviBeaconAPI/Controllers/NavigationController.cs
+++ SuperNaviBeaconAPI/SuperNaviBeaconAPI/Controllers/NavigationController.cs
@@ -4,26 +4,50 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using SuperNaviBeaconAPI.Models;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
 
 namespace SuperNaviBeaconAPI.Controllers
 {
     public class NavigationController : ApiController
     {
+        private static Dictionary<String, Session> connections = new Dictionary<String, Session>();
+
+        private CloudTable beaconTable = CloudStorageAccount.Parse(
+            CloudConfigurationManager.GetSetting("StorageConnectionString")).CreateCloudTableClient().GetTableReference("Beacon");
+
         // GET api/Navigation
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] {};
         }
 
         // GET api/Navigation/5
-        public string Get(int id)
+        public string Get(DtoBeacon beacon)
         {
             return "value";
         }
 
         // POST api/Navigation
-        public void Post([FromBody]string value)
+        public String Post(List<DtoBeacon> beacons)
         {
+            String ipAddress = Request.GetOwinContext().Request.RemoteIpAddress;
+            Session session;
+            if (connections.ContainsKey(ipAddress))
+            {
+                session = connections[ipAddress];
+            }
+            else
+            {
+                session = new Session();
+                connections[ipAddress] = session;
+            }
+
+            session.updateNewPosition(beacons);
+            return session.getDirection();
         }
 
         // PUT api/Navigation/5
