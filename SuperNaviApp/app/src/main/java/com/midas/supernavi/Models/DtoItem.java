06bddbc7348830796ef210b74c70f39a101174ed
hshe824 <hshe824@aucklanduni.ac.nz>
2016-10-10 14:13:26 +1300

    Added DTOItem

---
+++ SuperNaviApp/app/src/main/java/com/midas/supernavi/Models/DtoItem.java
@@ -0,0 +1,17 @@
+package com.midas.supernavi.Models;
+
+/**
+ * Created by hshe on 10/10/2016.
+ */
+
+
+public class DtoItem
+{
+    public DtoItem() { }
+    public String name;
+    public String supermarket ;
+    public int positionX = 0;
+    public int positionY = 0;
+    public String side;
+    
+}
